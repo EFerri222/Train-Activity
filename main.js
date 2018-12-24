9dcae8cd9129f1392775f4909b34d43ec7155d1c
EFerri222 <eferri222@gmail.com>
2018-12-23 23:35:59 -0500

    updated javascript to create Firebase event

--- main.js
+++ main.js
@@ -31,6 +31,51 @@ $(document).ready(function() {
         };
 
         database.ref().push(newTrain);
+
+        $("#train-name-input").val("");
+        $("#destination-input").val("");
+        $("#first-train-time-input").val("");
+        $("#frequency-input").val("");
     });
+
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+      
+        // Store everything into a variable.
+        var trainName = childSnapshot.val().name;
+        var trainDestination = childSnapshot.val().destination;
+        var firstTrainTime = childSnapshot.val().time;
+        var trainFrequency = childSnapshot.val().frequency;
+      
+        // Employee Info
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(firstTrainTime);
+        console.log(trainFrequency);
+      
+        // // Prettify the employee start
+        // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+      
+        // // Calculate the months worked using hardcore math
+        // // To calculate the months worked
+        // var empMonths = moment().diff(moment(empStart, "X"), "months");
+        // console.log(empMonths);
+      
+        // // Calculate the total billed rate
+        // var empBilled = empMonths * empRate;
+        // console.log(empBilled);
+      
+        // Create the new row
+        var newRow = $("<tr>").append(
+          $("<td>").text(trainName),
+          $("<td>").text(trainDestination),
+          $("<td>").text(trainFrequency),
+          $("<td>").text("Next Arrival"),
+          $("<td>").text("Minutes Away"),
+        );
+      
+        // Append the new row to the table
+        $("#train-table > tbody").append(newRow);
+      });
 
 });
